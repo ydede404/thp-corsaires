70c3ea00e1602f00342dadb7a1fabccf54f94e85
ydede404 <dakitse@gmail.com>
2017-08-02 17:20:48 +0200

    first commit

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get :home" do
+    get static_pages_:home_url
+    assert_response :success
+  end
+
+end
